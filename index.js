90a0bdc4c80b9742f8bda642d630e3f21ee33158
jacobgonzales <jagonz@live.com>
2023-06-29 15:31:13 -0500

    updated import contacts function

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-function createNewContact(){
-    /
+
+
+function addContact(){
+    const tbody = document.querySelector("tbody")
+    
+    
 };
+
+function importContacts(){
+    fetch('http://localhost:3000/contacts')
+    .then(response => response.json())
+    .then(contacts => {
+        contacts.forEach(contact => {
+            for (const key in contact) {
+                if (typeof key === number){
+                    console.log(contact[key])
+                }
+            // let tbody = document.querySelector("tbody")
+            // let tr = document.createElement('tr')
+            // let td = document.createElement('td')
+
+
+                console.log(contact[key])
+
+                //tr.id = contact.id
+
+
+            }
+            // console.log(contact)
+            // console.log(contact.id)
+            // console.log(contact.name)
+            // console.log(contact.occupation)
+            // console.log(contact.email)
+            // console.log(contact.phone)
+
+            // tbody.appendChild(tr)
+
+        });
+        console.log(contacts)
+    })
+    .catch(error => {
+        //handle error
+    })
+};
+
+importContacts()
